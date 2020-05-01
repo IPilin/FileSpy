a26462f5d1e199dee0e48bcb613074ed84b9ddd1
Botnavas <maksymmelkov@yandex.ru>
2020-05-01 18:06:26 +0300

    Test

--- FileSpy/Classes/Commands.cs
+++ FileSpy/Classes/Commands.cs
@@ -27,7 +27,5 @@
         public static int FileInfo { get; private set; } = 15;
         public static int FileData { get; private set; } = 16;
         public static int FileOK { get; private set; } = 17;
-
-        public static int Test {get; private set;} = 9999999;
     }
 }
